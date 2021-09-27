33c4c9edd11688fac2c35fbaee6a4d0a0e17d13a
Ray <ray@pop-os.localdomain>
2021-09-27 13:11:40 -0600

    adds User creation and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  resources :users
 end
